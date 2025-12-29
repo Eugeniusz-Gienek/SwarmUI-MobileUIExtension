c562c5714b9dd14bb7f908a263d843a889534d72
Eugeniusz <eugene@sky.community>
2025-12-29 02:21:35 +0100

    Metadata

--- MobileUIExtension.cs
+++ MobileUIExtension.cs
@@ -9,6 +9,12 @@ namespace Eugeniusz.Extensions.MobileUIExtension
         {
             Logs.Info("MobileUIExtension Version 0.1 started.");
             StyleSheetFiles.Add("Assets/mobileUI.css");
+            this.Version = "0.1";
+            this.ExtensionAuthor = "Eugeniusz-Gienek";
+            this.License = "MIT";
+            this.Description = "Mobile-friendly UI";
+            var list = new List<string> { "UI" };
+            this.Tags = list.ToArray();
         }
 
         public override void OnInit()
